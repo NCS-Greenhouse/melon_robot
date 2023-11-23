ed7debfa4526574ea7a8f3b0abb4b5b2b75f575d
ShengDao Du <r08921101@ntu.edu.tw>
2023-11-23 05:40:57 +0000

    Feature/Accessing credentials from conf file

--- src/robot_control_pkg/scripts/srv_image_uploadar.py
+++ src/robot_control_pkg/scripts/srv_image_uploadar.py
@@ -4,15 +4,12 @@ Created on Fri Aug 25 21:28:21 2023
 
 @author: jun94
 @Modified By: ShengDao Du
-
+@Maintainer: ShengDao Du
 """
 
-import pymongo
-import cv2
 import numpy as np
-import os
-import base64
-import rospy
+import pymongo, cv2
+import rospy, rospkg, configparser, base64, os
 from greenhouse_arm_pkg.srv import upload_img, upload_imgRequest, upload_imgResponse
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -20,13 +17,40 @@ from cv_bridge import CvBridge, CvBridgeError
 class ImageUploader():
     def __init__(self) -> None:
         rospy.init_node("image_uploader")
+        rospy.on_shutdown(self.close_connection)
+
+        #read configuration file
+        # Source: https://docs.python.org/3/library/configparser.html
+        self.conf = configparser.ConfigParser() 
+        self.rospack = rospkg.RosPack()
+        #Add Try-Except Clause
+        self.package_root = self.rospack.get_path("robot_control_pkg") #Get current package absolute location
+        self.conf.read(self.package_root + "/package.conf") # Try to load this configuration file
+        if((self.conf.get('forge.mango', 'mango-credentials',fallback='err')) == 'err' \
+            or self.conf.get('forge.mango', 'mango-db-cli',fallback='err')  == 'err'):
+            # OR not 'forge.mango' in self.conf
+            rospy.logerr("Configuration Loading Fail.")
+            rospy.signal_shutdown("Configuration Loading Fail.")
+            return
+        else:
+            pass
+        rospy.loginfo("Configuration Loaded.")
+        #Check Configuration File
         self.upload_haldlar = rospy.Service('upload_img', upload_img, self.upload_image)
-        self.cli = pymongo.MongoClient("mongodb://65feb189-94b4-4b4e-9c48-9f41752e8a2b:2aDcNa6FiDmRf293IoBnjBHy@apps-mongodb-single-9f379c12-32ce-477b-8871-d0436bdd634d-pub.education.wise-paas.com:27017/a745f4c1-dac0-4af1-a27d-84ae4da47ec5")
-        self.db=self.cli["a745f4c1-dac0-4af1-a27d-84ae4da47ec5"]
+        self.cli = pymongo.MongoClient(self.conf['forge.mango']['mango-credentials'])
+        self.db=self.cli[self.conf['forge.mango']['mango-db-cli']]
         self.collection=self.db["ctlp_img_rt"]
         self.collection.delete_many({})
         self.bridge = CvBridge()
         pass
+    
+    def close_connection(self):
+        try:
+            #if mango is connected
+            self.cli.close()
+            rospy.loginfo("Disconnect from Mango Server.")
+        except:
+            pass
 
     def read_img_b64(self, cvimg):
         b64_str = cv2.imencode(".jpg",cvimg)[1].tobytes()
