c6e1401720d33752f6d4383040130aa681f7c893
ShengDaoDu <duchengdao@gmail.com>
2023-12-22 00:53:54 +0800

    this is the example of automatically generated script

---
+++ src/robot_control_pkg/scripts/utils/_aruco_boards.py
@@ -0,0 +1,55 @@
+# This script is automatically generated. 
+# Modifying this code is not recommanded
+import cv2
+import numpy as np
+def get_boards():
+	return boards
+
+def get_dict():
+	return aruco_dict
+
+aruco_dict=cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_250)
+board_corners_0 = [np.array([[-0.0603,  0.0286,  0.    ], [-0.0032,  0.0286,  0.    ], [-0.0032, -0.0286,  0.    ], [-0.0603, -0.0286,  0.    ]],dtype=np.float32), np.array([[ 0.0032,  0.0286,  0.    ], [ 0.0603,  0.0286,  0.    ], [ 0.0603, -0.0286,  0.    ], [ 0.0032, -0.0286,  0.    ]],dtype=np.float32),]
+board_corners_1 = [np.array([[-0.0603,  0.0286,  0.    ], [-0.0032,  0.0286,  0.    ], [-0.0032, -0.0286,  0.    ], [-0.0603, -0.0286,  0.    ]],dtype=np.float32), np.array([[ 0.0032,  0.0286,  0.    ], [ 0.0603,  0.0286,  0.    ], [ 0.0603, -0.0286,  0.    ], [ 0.0032, -0.0286,  0.    ]],dtype=np.float32),]
+board_corners_2 = [np.array([[-0.0603,  0.0286,  0.    ], [-0.0032,  0.0286,  0.    ], [-0.0032, -0.0286,  0.    ], [-0.0603, -0.0286,  0.    ]],dtype=np.float32), np.array([[ 0.0032,  0.0286,  0.    ], [ 0.0603,  0.0286,  0.    ], [ 0.0603, -0.0286,  0.    ], [ 0.0032, -0.0286,  0.    ]],dtype=np.float32),]
+board_corners_3 = [np.array([[-0.0603,  0.0286,  0.    ], [-0.0032,  0.0286,  0.    ], [-0.0032, -0.0286,  0.    ], [-0.0603, -0.0286,  0.    ]],dtype=np.float32), np.array([[ 0.0032,  0.0286,  0.    ], [ 0.0603,  0.0286,  0.    ], [ 0.0603, -0.0286,  0.    ], [ 0.0032, -0.0286,  0.    ]],dtype=np.float32),]
+board_corners_4 = [np.array([[-0.0603,  0.0286,  0.    ], [-0.0032,  0.0286,  0.    ], [-0.0032, -0.0286,  0.    ], [-0.0603, -0.0286,  0.    ]],dtype=np.float32), np.array([[ 0.0032,  0.0286,  0.    ], [ 0.0603,  0.0286,  0.    ], [ 0.0603, -0.0286,  0.    ], [ 0.0032, -0.0286,  0.    ]],dtype=np.float32),]
+board_corners_5 = [np.array([[-0.0603,  0.0286,  0.    ], [-0.0032,  0.0286,  0.    ], [-0.0032, -0.0286,  0.    ], [-0.0603, -0.0286,  0.    ]],dtype=np.float32), np.array([[ 0.0032,  0.0286,  0.    ], [ 0.0603,  0.0286,  0.    ], [ 0.0603, -0.0286,  0.    ], [ 0.0032, -0.0286,  0.    ]],dtype=np.float32),]
+board_corners_6 = [np.array([[-0.0603,  0.0286,  0.    ], [-0.0032,  0.0286,  0.    ], [-0.0032, -0.0286,  0.    ], [-0.0603, -0.0286,  0.    ]],dtype=np.float32), np.array([[ 0.0032,  0.0286,  0.    ], [ 0.0603,  0.0286,  0.    ], [ 0.0603, -0.0286,  0.    ], [ 0.0032, -0.0286,  0.    ]],dtype=np.float32),]
+board_corners_7 = [np.array([[-0.0603,  0.0286,  0.    ], [-0.0032,  0.0286,  0.    ], [-0.0032, -0.0286,  0.    ], [-0.0603, -0.0286,  0.    ]],dtype=np.float32), np.array([[ 0.0032,  0.0286,  0.    ], [ 0.0603,  0.0286,  0.    ], [ 0.0603, -0.0286,  0.    ], [ 0.0032, -0.0286,  0.    ]],dtype=np.float32),]
+board_corners_8 = [np.array([[-0.0603,  0.0286,  0.    ], [-0.0032,  0.0286,  0.    ], [-0.0032, -0.0286,  0.    ], [-0.0603, -0.0286,  0.    ]],dtype=np.float32), np.array([[ 0.0032,  0.0286,  0.    ], [ 0.0603,  0.0286,  0.    ], [ 0.0603, -0.0286,  0.    ], [ 0.0032, -0.0286,  0.    ]],dtype=np.float32),]
+board_corners_9 = [np.array([[-0.0603,  0.0286,  0.    ], [-0.0032,  0.0286,  0.    ], [-0.0032, -0.0286,  0.    ], [-0.0603, -0.0286,  0.    ]],dtype=np.float32), np.array([[ 0.0032,  0.0286,  0.    ], [ 0.0603,  0.0286,  0.    ], [ 0.0603, -0.0286,  0.    ], [ 0.0032, -0.0286,  0.    ]],dtype=np.float32),]
+
+board_ids_0=np.array([[0],[1]], dtype = np.int32)
+board_ids_1=np.array([[2],[3]], dtype = np.int32)
+board_ids_2=np.array([[4],[5]], dtype = np.int32)
+board_ids_3=np.array([[6],[7]], dtype = np.int32)
+board_ids_4=np.array([[8],[9]], dtype = np.int32)
+board_ids_5=np.array([[10],[11]], dtype = np.int32)
+board_ids_6=np.array([[12],[13]], dtype = np.int32)
+board_ids_7=np.array([[14],[15]], dtype = np.int32)
+board_ids_8=np.array([[16],[17]], dtype = np.int32)
+board_ids_9=np.array([[18],[19]], dtype = np.int32)
+
+board_0 = cv2.aruco.Board(board_corners_0,aruco_dict,board_ids_0)
+board_1 = cv2.aruco.Board(board_corners_1,aruco_dict,board_ids_1)
+board_2 = cv2.aruco.Board(board_corners_2,aruco_dict,board_ids_2)
+board_3 = cv2.aruco.Board(board_corners_3,aruco_dict,board_ids_3)
+board_4 = cv2.aruco.Board(board_corners_4,aruco_dict,board_ids_4)
+board_5 = cv2.aruco.Board(board_corners_5,aruco_dict,board_ids_5)
+board_6 = cv2.aruco.Board(board_corners_6,aruco_dict,board_ids_6)
+board_7 = cv2.aruco.Board(board_corners_7,aruco_dict,board_ids_7)
+board_8 = cv2.aruco.Board(board_corners_8,aruco_dict,board_ids_8)
+board_9 = cv2.aruco.Board(board_corners_9,aruco_dict,board_ids_9)
+boards = []
+boards.append(board_0)
+boards.append(board_1)
+boards.append(board_2)
+boards.append(board_3)
+boards.append(board_4)
+boards.append(board_5)
+boards.append(board_6)
+boards.append(board_7)
+boards.append(board_8)
+boards.append(board_9)
+
