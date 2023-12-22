de736585e6e770482c7fe3ddf4388aa9f6554799
ShengDaoDu <duchengdao@gmail.com>
2023-12-23 00:01:29 +0800

    Feature: this file will initialize the ros paramaters

--- src/robot_control_pkg/scripts/utils/utils.py
+++ src/robot_control_pkg/scripts/utils/utils.py
@@ -2,18 +2,23 @@
 import GPUtil
 import rospy
 
+ARUCO_CAMERA_TYPE = 'color'
 
-class python_utils:
-    def __init__(self,argc) -> None:
+class parameter_initializer:
+    def __init__(self) -> None:
         rospy.init_node("python_utils",anonymous=True)
-        if(argc == 'check-gpu'):
-            self.__check_gpu_status__()
-    
+        self.__check_gpu_status__()
+        self.initialize_parameters()
+
     def __check_gpu_status__(self)->None:
         # print()
         gpu_availability = len(GPUtil.getAvailable())
         rospy.set_param("gpu_number",gpu_availability)
         rospy.loginfo("Set the parameter 'gpu_number' to %s.",gpu_availability)
+    
+    def initialize_parameters(self)->None:
+        rospy.set_param("/aruco_detector/camera_type",ARUCO_CAMERA_TYPE)
 
-python_utils("check-gpu")
+
+parameter_initializer()
 
